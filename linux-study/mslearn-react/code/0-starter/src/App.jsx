409d525f1dc833c0eba13ddb6e54702a3dee14a3
A2GMer <52153162+A2GMer@users.noreply.github.com>
2023-08-30 22:54:33 +0900

    finish https://learn.microsoft.com/ja-jp/training/modules/react-get-started/10-summary

--- linux-study/mslearn-react/code/0-starter/src/App.jsx
+++ linux-study/mslearn-react/code/0-starter/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css'
 
 function App() {
     return (
         <article>
             <h1>Recipe Manager</h1>
+            <RecipeTitle />
+            <div>{ Date.now() }</div>
         </article>
     )
 }
